7b5bf43b4d7bf37e9483f25612dce8d82a437a6c
Bohdan Kolecek <bohdan.kolecek@apollo13.cz>
2015-07-13 10:40:07 +0200

    US-347 Access Docker host IP address

--- bin/run.sh
+++ bin/run.sh
@@ -7,7 +7,8 @@ if [ ! -f /.rabbitmq_password_set ]; then
 fi
 
 # register current IP address into config-service
-config-service-set "${SERVER_NAME}_host" "$CONTAINER_IPV4_ADDRESS"
+config-service-set "${SERVER_NAME}_host" "${HOST_IPV4_ADDRESS:-${CONTAINER_IPV4_ADDRESS}}"
+config-service-set "${SERVER_NAME}_hostPrivate" "$CONTAINER_IPV4_ADDRESS"
 config-service-set "${SERVER_NAME}_port" "5672"
 
 # make rabbit own its own files
